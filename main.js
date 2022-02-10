ad090ad8318ac26da2deb4ca8e9ca2ed3bc57683
CamiloMonje <lumonje12@gmail.com>
2022-02-10 15:56:20 -0500

    Se creo la clase Ball

--- main.js
+++ main.js
@@ -17,6 +17,22 @@
     }
 })();
 
+(function(){
+    self.Ball = function(x,y,radius,board){ // Se crea la clase BALL
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
+    }
+
+
+})();
+
+
 (function(){ //Clase para crear objetos dentro del board.
     self.Bar = function(x,y,width,height,board){
         this.x = x;
@@ -42,7 +58,7 @@
     }
 })();
 
-(function(){  //Se crea clase para dibujar en pantalla --> Controlador.
+(function(){  //Se crea clase para dibujar en pantalla.
     self.BoardView = function(canvas,board){
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -52,22 +68,34 @@
     }
 
     self.BoardView.prototype = { //Se crea para llamar la funcion draw y parsarle los elementos.
+        clean: function(){
+            this.ctx.clearRect(0,0,board.width,board.height);
+        },
         draw: function(){
             for(var i = this.board.elements.length -1; i>= 0; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             }
+        },
+        play : function(){
+            this.clean();
+            this.draw();
         }
     }
 
-    function draw(ctx,element){ //Dibuja el elemento.
-        if(element !== null && element.hasOwnProperty("kind")){    
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                    break;
+    function draw(ctx,element){ //Dibuja el elemento.        
+        switch(element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y, element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
             }
-        }
+    
     }
 
     
@@ -75,25 +103,36 @@
 
 var board = new Board(800,400); //Se instancia y se le dan los parametros de la clase board
 var bar = new Bar(20,100,40,100,board); //Se crea el objeto bar para crear el rectangulo
-var bar = new Bar(735,100,40,100,board); //Se crea el objeto bar para crear el rectangulo
+var bar_2 = new Bar(735,100,40,100,board); //Se crea el objeto bar para crear el rectangulo
 var canvas = document.getElementById('canvas'); //Se obtiene el elemento canvas del html
 var board_view = new BoardView(canvas,board); //Se instancia la clase BoardView y se utiliza el controlador.
+var ball = new Ball(350,100,10, board); //Se instancia la clase Ball 
 
 document.addEventListener("keydown",function(ev){
+    ev.preventDefault();
     if(ev.keyCode == 38) {
         bar.up();
     }else if (ev.keyCode == 40){
         bar.down();
     }
-    console.log(bar.toString());
+    // Tecla W
+    else if (ev.keyCode == 87){ 
+        bar_2.up();
+    }//Tecla S
+    else if (ev.keyCode == 83){
+        bar_2.down();
+    }
+
+
+    console.log("1. " + bar);
+    console.log("2. " + bar_2);
 });
 
-addEventListener("load", main); // Se ejecuta la función main
+window.requestAnimationFrame(controller);
 
-function main(){
-    console.log("Hola Mundo");
-    console.log(board);
-    board_view.draw();
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 
 }
 
